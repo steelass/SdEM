e89fd0ef4527dd7416f16c42c87a7a9ecfb6535a
Jack Resci <giacomoresci@gmail.com>
2016-05-09 18:21:55 +0200

    Add bitwriter.h numeri5bit.cpp image.h Gradient.cpp

---
+++ gradient_PGM/gradient.cpp
@@ -0,0 +1,59 @@
+/*
+Si scriva un programma che generi un’immagine a livelli di grigio di 256×256 pixel, in cui la prima riga è
+costituita di 256 valori 0, la seconda di 256 valori 1, la terza di 256 valori 2 e così via. Si salvi l’immagine
+nei formati PGM e plain PGM semplificati, descritti precedentemente. Verificare che l’immagine sia
+visualizzabile in XnView. L’immagine dovrebbe apparire come un gradiente dal nero al bianco dall’alto
+verso il basso.
+*/
+
+#include <cstdint>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <iterator>
+
+#include "image.h"
+
+;
+using namespace std;
+
+int L = 256;
+int H = 256;
+
+
+
+void makeGradient(image<uint8_t>& img) {
+	for(size_t i = 0; i < img.h() ; ++i) {
+		for(size_t j = 0; j < img.l() ; ++j) {
+			img(j,i) = i;
+		}
+	}
+
+}
+
+
+int main(int argc, char* argv[]){
+	cout << "GRADIENT PGM\n";
+	cout << "...Apertura stream di output...\n";
+	if(argc<2) {
+		cout << "Passare almeno un parametro!\n";
+		return EXIT_FAILURE;
+	}
+	ofstream out(argv[1],ios::binary);
+	ofstream out_p(argv[2],ios::binary);
+	if(!out || ! out_p){
+		cout << "Errore apertura file di output!\n";
+		return EXIT_FAILURE;
+	}
+	cout << "...Creazione Immagine Gradiente...\n";
+	image<uint8_t> i(H,L);
+	makeGradient(i);
+	cout << "...Scrittura formato PGM...\n";
+	i.writePGM(out);
+	cout << "...Scrittura formato PGM plain...\n";
+	i.writePGM(out_p,true);
+
+	return EXIT_SUCCESS;
+
+
+}
