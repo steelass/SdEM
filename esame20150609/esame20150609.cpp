8b7c1b0985894264062860610aad24d596844c61
Jack Resci <giacomoresci@gmail.com>
2016-06-10 00:04:06 +0200

    Update esame 2016 06 09, with loop filter but same problems with the interpolation.

--- esame20150609/esame20150609.cpp
+++ esame20150609/esame20150609.cpp
@@ -13,6 +13,7 @@ class RGB {
 	uint8_t B;
 
 public:
+	RGB() : R(0), G(0), B(0) {}
 	RGB(uint8_t r, uint8_t g, uint8_t b) : R(r), G(g), B(b) {}
 	
 	uint8_t r() {
@@ -48,7 +49,7 @@ class image {
 	vector<T> data;
 		
 public:
-	image(size_t max, size_t H, size_t L) : max_g(max), h(H), l(L) {}
+	image(size_t max, size_t H, size_t L) : max_g(max), h(H), l(L), data(H*L){}
 	
 	const size_t MaxG() {
 		return max_g;
@@ -77,12 +78,19 @@ public:
 		data.swap(d);
 	}
 
-
-	T operator()(size_t x, size_t y) {
-		return data[y * l + x];
+	vector<T> getData() {
+		return data;
 	}
 
 
+
+	T& operator()(size_t x, size_t y) {
+		return data[y * l + x];
+	}
+
+	const T& operator()(size_t x, size_t y) const {
+		return data[y * l + x];
+	}
 	char* dataptr() {
 		return reinterpret_cast<char*>(&data[0]);
 	}
@@ -103,6 +111,54 @@ public:
 };
 
 uint16_t make16(char c1,char c2) {
+	uint16_t el = 0;
+	size_t h = 8;
+	while(h-->0) {
+		el = (el << 1) | (c1 >> h) & 1;
+	}
+	h = 8;
+	while(h-->0) {
+		el = (el << 1) | (c2 >> h) & 1;
+	}
+
+	return el;
+}
+
+image<uint8_t> lp(image<uint8_t> i) {
+	size_t w =i.L();
+	size_t h = i.H();
+	image<uint8_t> out(255, h,w);
+	image<uint8_t> out2(255, h,w);
+
+	for(size_t r = 0; r < h; ++r) {
+		out(0,r) = i(0,r);
+		for (size_t c = 1; c < w - 1; ++c) {
+			out(c,r) = i(c-1,r)/4 + i(c,r)/2 + i(c+1,r)/4;
+		}
+		out(w-1,r) = i(w-1,r);
+	}
+
+
+	for(size_t i = 0;i < w; ++i) {
+		out2(i,0) = out(i,0);
+	}
+	for(size_t r = 1;r < h-1; ++r) {
+		for(size_t c = 0 ; c < w; ++c) {
+			out2(c,r) = out(c,r-1)/4 + out(c,r)/2 + out(c,r+1)/4;
+		}
+	}
+		
+	for (size_t c = 0; c < w; ++c) {
+		out2(c, h - 1) = out(c, h - 1);
+	}
+
+	return out;
+
+}
+
+
+
+uint16_t make16(uint8_t c1,uint8_t c2) {
 	uint16_t el = 0;
 	size_t h = 8;
 	while(h-->0) {
@@ -142,14 +198,18 @@ bool readPGM(ifstream &in,vector<uint16_t> &v,image<uint8_t> &img) {
 	img.setL(l);
 	img.setH(h);
 	char c1,c2;
+	uint8_t cc,ccc;
 	uint16_t el;
 	while(!in.eof()) {
 
 		in.get(c2);
 		in.get(c1);
+		//in >> ccc;
+		//in >> cc;
 		if(in.eof()) {
 			break;
 		}
+		//el = make16(cc,ccc);
 		el = make16(c1,c2);
 		v.push_back(el);
 	}
@@ -171,6 +231,7 @@ void popolaRGB(vector<uint8_t> &in, vector<RGB> &out,size_t h, size_t l) {
 	size_t x;
 	bool r = true;
 	for(size_t y = 0; y < h; ++y) {
+		r = ((y%2==0) ? true:false);
 		for(x = 0; x < l; ++x) {
 			if(r){
 				if(x%2==0) {
@@ -191,18 +252,18 @@ void popolaRGB(vector<uint8_t> &in, vector<RGB> &out,size_t h, size_t l) {
 
 
 		}
-		r = (r ? false:true);
+		//r = (r ? false:true);
 	}
 }
 
 
 void interpolazioneV(vector<uint8_t> &v,size_t l, size_t h,vector<RGB> &out){
-	uint8_t G5,G4,G2,G6,G8;
-	uint8_t X1,X3,X7,X9,X5;
-	uint8_t AH, AV;
+	uint16_t G5,G4,G2,G6,G8;
+	uint16_t X1,X3,X7,X9,X5;
+	uint16_t AH, AV;
 	size_t x;
 	for(size_t y = 0; y < h; ++y) {
-		for(x = ( (y%2==0) ? 0:1 ); x < l; x+=2) {
+		for(x = ((y%2==0) ? 0:1 ); x < l; x+=2) {
 			X5 = v[y * l +x];
 			
 			X1 = ((y<2) ? v[ ( y + 2 ) * l + x ] : v[ ( y - 2 ) * l + x ]);
@@ -247,8 +308,15 @@ void interpolazioneV(vector<uint8_t> &v,size_t l, size_t h,vector<RGB> &out){
 			else {
 				out[y*l+x].setB(X5);
 			}*/
+			/*if(G5 > 65534 || G5 < 0){
+				cout << "e: " << G5 << " ";
+				G5 = 0;
+			}
+			else if (G5>255) {
+				G5 = 255;
+			}*/
 
-			out[y*l+x].setG(G5);
+			out[y*l+x].setG((G5<0) ? 0:G5);
 		}
 
 	}
@@ -256,30 +324,64 @@ void interpolazioneV(vector<uint8_t> &v,size_t l, size_t h,vector<RGB> &out){
 }
 
 void interpolazione2(vector<uint8_t> &v,size_t l, size_t h,vector<RGB> &out){
-	uint8_t G1,G3,G7,G9,G5,G4,G2,G6,G8;
-	uint8_t X1,X3,X7,X9,X5;
-	uint8_t AN, AP;
+	uint16_t G1,G3,G7,G9,G5,G4,G2,G6,G8;
+	uint16_t X1,X3,X7,X9,X5;
+	uint16_t AN, AP;
 	size_t xi;
-	uint8_t r,b;
+	uint16_t r,b;
 	cout << "Prima parte\n";
-	for(size_t y = 0; y < h; ++y) {
+	size_t y;
+	for(y = 0; y < h; ++y) {
 		for(xi = ( (y%2==0) ? 1:0 ); xi < l; xi+=2) {
 			r = ((xi==0) ? v[y*l+xi+1] : ((xi==l-1) ? v[y*l+xi-1] : (v[y*l+xi-1] + v[y*l+xi+1])/2));
 			b = ((y==0) ? v[(y+1)*l+xi] : ((y==h-1) ? v[(y-1)*l+xi] : (v[(y-1)*l+xi] + v[(y+1)*l+xi])/2));
 
-			out[y*l+xi].setR(r);
-			out[y*l+xi].setB(b);
+			
+			/*if(r>65534) {
+				r = 0;
+			}
+			if(b>65534) {
+				b = 0;
+			}
+			if(r > 255) {
+				r = 255;
+			}
+			if( b > 255) {
+				b = 255;
+			}*/
+
+			out[y*l+xi].setR((r<0) ? 0:r);
+			out[y*l+xi].setB((b<0) ? 0:b);
 		}
 	}
 
 	cout << "Seconda parte\n";
 	size_t x;
 	bool red = true;
-	for(size_t y = 0; y < h; ++y) {
+	for(y = 0; y < h; ++y) {
+		red = ((y%2==0) ? true: false);
 		for(x = ( (y%2==0) ? 0:1 ); x < l; x+=2) {
-			G5 = v[y * l +x];
-			red = ((y%2==0) ? true: false);
+			G5 = out[y * l +x].g();
+			
 
+			///*
+			X1 =((x<1 && y<1) ? v[(y+1)*l+x+1] : ((x<1) ? v[(y-1)*l+x+1] : ((y<1) ? v[(y+1)*l+x-1] : v[(y-1)*l+x-1])));
+			G1 =((x<1 && y<1) ? out[(y+1)*l+x+1].g() : ((x<1) ? out[(y-1)*l+x+1].g() : ((y<1) ? out[(y+1)*l+x-1].g() : out[(y-1)*l+x-1].g())));
+			G2 = ((y<1) ? out[ ( y + 1 ) * l + x ].g() : out[ ( y - 1 ) * l + x ].g());
+
+			X3 = ((x>l-2 && y==0) ? v[(y+1)*l+x-1] : ((y<1) ? v[(y+1)*l+x+1] : ((x>l-2) ? v[(y-1)+x-1] : v[(y-1)*l+x+1])));
+			G3 = ((x>l-2 && y==0) ? out[(y+1)*l+x-1].g() : ((y<1) ? out[(y+1)*l+x+1].g() : ((x>l-2) ? out[(y-1)+x-1].g() : out[(y-1)*l+x+1].g())));
+		
+			G4 = ((x<1) ? out[ y * l + x + 1 ].g() : out[ y * l + x - 1 ].g());
+			G6 = ((x>l-2) ? out[ y * l + x - 1 ].g() : out[ y * l + x + 1 ].g());
+			X7 = ((x>l-2 && y>h-2) ? v[(y-1)*l+x+1] : ((y>h-2) ? v[(y-1)*l+x-1] : ((x<1 && y<1) ? v[(y+1)*l+x+1] : v[(y+1)*l+x-1])));
+			G7 = ((x==0 && y>h-2) ? out[(y-1)*l+x+1].g() : ((y>h-2) ? out[(y-1)*l+x-1].g() : ((x<1 && y<1) ? out[(y+1)*l+x+1].g() : out[(y+1)*l+x-1].g())));
+			G8 = ((y>h-2) ? out[ ( y - 1 ) * l + x ].g() : out[ ( y + 1 ) * l + x ].g());
+			X9 = ((x>l-2 && y>h-2) ? v[(y-1)*l+x-1] : ((y>h-2) ? v[(y-1)*l+x+1] : ((x>l-2) ? v[(y+1)*l+x-1] : v[(y+1)*l+x+1])));
+			G9 = ((x>l-2 && y>h-2) ? out[(y-1)*l+x-1].g() : ((y>h-2) ? out[(y-1)*l+x+1].g() : ((x>l-2) ? out[(y+1)*l+x-1].g() : out[(y+1)*l+x+1].g())));
+			
+			//*/
+			/*
 			X1 = ((y<2) ? v[ ( y + 2 ) * l + x ] : v[ ( y - 2 ) * l + x ]);
 			G1 = ((y<2) ? out[ ( y + 2 ) * l + x ].g() : out[ ( y - 2 ) * l + x ].g());
 			G2 = ((y<1) ? out[ ( y + 1 ) * l + x ].g() : out[ ( y - 1 ) * l + x ].g());
@@ -293,6 +395,8 @@ void interpolazione2(vector<uint8_t> &v,size_t l, size_t h,vector<RGB> &out){
 			G6 = ((x>l-2) ? out[ y * l + x - 1 ].g() : out[ y * l + x + 1 ].g());
 			X7 = ((x>l-3) ? v[ y * l + x - 2 ] : v[ y * l + x + 2 ]);
 			G7 = ((x>l-3) ? out[ y * l + x - 2 ].g() : out[ y * l + x + 2 ].g());
+			
+			//*/
 			 
 		
 
@@ -310,18 +414,20 @@ void interpolazione2(vector<uint8_t> &v,size_t l, size_t h,vector<RGB> &out){
 					X5 = (X1 + X3 + X7 + X9)/4 + (G5 - G1 + G5 - G3 + G5 - G7 + G5 - G9)/8;
 				}
 			}
-			/*if(x%2==1){
-				out[y*l+x].setR(X5);
+			
+			/*if ( X5 > 65534) {
+				X5 = 0;
 			}
-			else {
-				out[y*l+x].setB(X5);
+			if (X5 > 255) {
+				X5 = 255;
 			}*/
 
+
 			if ( red) {
-				out[y*l+x].setB(X5);
+				out[y*l+x].setB((X5<0) ? 0:X5);
 			}
 			else{	
-				out[y*l+x].setR(X5);
+				out[y*l+x].setR((X5<0) ? 0:X5);
 			}
 
 		}
@@ -359,8 +465,11 @@ int main(int argc,char **argv) {
 	newMax /= 256;
 	i.setMax(newMax);
 	cout << "Scrivo la nuova immagine\n";
+	
 	i.setData(vv);
-	i.write(out);
+	image<uint8_t> lop = lp(i);
+	
+	lop.write(out);
 	size_t l = i.L();
 	size_t h = i.H();
 	cout << "Inizializzo il vettore RGB\n";
@@ -372,19 +481,16 @@ int main(int argc,char **argv) {
 		++c;
 	}
 	cout << "Popolo il vettore RGB con i valori del pattern\n";
-	popolaRGB(vv,rgb,h,l);
+	popolaRGB(lop.getData(),rgb,h,l);
 	
 	cout << "Interpolazione verde\n";
 	interpolazioneV(vv,i.L(),i.H(),rgb);
-	image<RGB> i2(newMax, h,l);
-	i2.setData(rgb);
-	i2.write(out2,false);
 	cout << "Interpolazione parte 2\n";
 	interpolazione2(vv,l,h,rgb);
 	cout << "Creazione Immagine\n";
-	/*image<RGB> i2(newMax, h,l);
+	image<RGB> i2(newMax, h,l);
 	i2.setData(rgb);
-	i2.write(out2,false);*/
+	i2.write(out2,false);
 	return EXIT_SUCCESS;
 }
 
